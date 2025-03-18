7d56b5aa14503ef8fb50d4cb88d3ef261b3a7e75
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-03-18 17:45:31 +0900

    Initial commit

---
+++ test/line-scheduler-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LineSchedulerSample from '../lib/line-scheduler-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/line-scheduler-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LineSchedulerSample.LineSchedulerSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
